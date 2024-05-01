1b61c9e14835f564b142530d8a2d3a8f0f07ebba
MAHossain1 <mahpro110@gmail.com>
2024-05-01 12:30:07 +0600

    Setup logger using winston done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { logger, errorLogger } from './shared/logger';
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log('🚀 Database is connected successfully.');
+    logger.info('👌 Database is connected successfully.');
 
     app.listen(config.port, () => {
-      console.log(`University Management app listening on port ${config.port}`);
+      logger.info(
+        `🚀 University Management app listening on port ${config.port}`
+      );
     });
   } catch (error) {
-    console.log('Failed to connect with database', error);
+    errorLogger.error('🧐 Failed to connect with database', error);
   }
 }
 
