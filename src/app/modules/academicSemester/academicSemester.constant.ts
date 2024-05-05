1fc7053221b5b537cc66bedb054955d966125617
MAHossain1 <mahpro110@gmail.com>
2024-05-05 19:16:50 +0600

    Improved semester interfaces and schemas

---
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -0,0 +1,32 @@
+import {
+  IAcademicSemesterCodes,
+  IAcademicSemesterTitles,
+  IMonths,
+} from './academicSemester.interface';
+
+export const academicSemesterTitles: IAcademicSemesterTitles[] = [
+  'Autumn',
+  'Summer',
+  'Fall',
+];
+
+export const academicSemesterMonths: IMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const academicSemesterCodes: IAcademicSemesterCodes[] = [
+  '01',
+  '02',
+  '03',
+];
---
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-semester',
+  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema)
+);
