c6148fa5595c0b05260dd41fe7ba6d083ab1ab86
MAHossain1 <mahpro110@gmail.com>
2024-05-06 06:45:19 +0600

    Validated academic semester title with required code

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,19 @@
+import httpStatus from 'http-status';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
+import ApiError from '../../../errors/ApiError';
 
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `Invalid code:${payload.code} for ${payload.title}`
+    );
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
