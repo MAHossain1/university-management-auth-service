7b362142759a05b6bdbb5cf3334ff6d4a6d65872
MAHossain1 <mahpro110@gmail.com>
2024-05-09 11:25:12 +0600

    Created & implemented academicFaculty controller, service

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import { AcademicFacultyService } from './academicFaculty.service';
+import sendResponse from '../../../shared/sendResponse';
+import { IAcademicFaculty } from './academicFaculty.interface';
+import httpStatus from 'http-status';
+
+const createFaculty = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicFacultyService.createFaculty(req.body);
+
+  sendResponse<IAcademicFaculty>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty created successfully.',
+    data: result,
+  });
+});
+
+export const AcademicFacultyController = {
+  createFaculty,
+};
--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -7,12 +7,18 @@ import {
 const academicFacultySchema = new Schema<
   IAcademicFaculty,
   AcademicFacultyModel
->({
-  title: {
-    type: String,
-    required: true,
+>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
 
 export const AcademicFaculty = model<IAcademicFaculty, AcademicFacultyModel>(
   'AcademicFaculty',
