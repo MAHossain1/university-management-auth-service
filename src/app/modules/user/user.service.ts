82aa130b6ee3d86f9983b9804fe17ec0fa937f9d
MAHossain1 <mahpro110@gmail.com>
2024-05-12 16:23:34 +0600

    Created faculty with user and added Academic management.

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -6,9 +6,11 @@ import { IStudent } from '../student/student.interface';
 import { IUser } from './user.interface';
 import { User } from './user.model';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
-import { generateStudentId } from './user.utils';
+import { generateFacultyId, generateStudentId } from './user.utils';
 import mongoose from 'mongoose';
 import { Student } from '../student/student.model';
+import { IFaculty } from '../faculty/faculty.interface';
+import { Faculty } from '../faculty/faculty.model';
 
 const createStudent = async (
   student: IStudent,
@@ -82,6 +84,72 @@ const createStudent = async (
   return newUserAllData;
 };
 
+// create faculty with user
+const createFaculty = async (
+  faculty: IFaculty,
+  user: IUser
+): Promise<IUser | null> => {
+  // Set default password
+  if (!user.password) {
+    user.password = config.default_faculty_password as string;
+  }
+
+  // Set role
+  user.role = 'faculty';
+
+  let newUserAllData = null;
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    const id = await generateFacultyId();
+    // Append id to user and faculty
+    user.id = id;
+    faculty.id = id;
+
+    const newFaculty = await Faculty.create([faculty], { session });
+
+    if (!newFaculty.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create faculty.');
+    }
+
+    // set faculty _id into user
+    user.faculty = newFaculty[0]._id;
+
+    const newUser = await User.create([user], { session });
+
+    if (!newUser.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create user.');
+    }
+
+    newUserAllData = newUser[0];
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
+  }
+
+  if (newUserAllData) {
+    newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+      path: 'faculty',
+      populate: [
+        {
+          path: 'academicDepartment',
+        },
+        {
+          path: 'academicFaculty',
+        },
+      ],
+    });
+  }
+
+  return newUserAllData;
+};
+
 export const UserService = {
   createStudent,
+  createFaculty,
 };
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -26,25 +26,43 @@ export const generateStudentId = async (
   return incrementedId;
 };
 
-const findLastFacultyId = async (): Promise<string | undefined> => {
-  const lastFacultyId = await User.findOne(
-    { role: 'faculty' },
-    { id: 1, _id: 0 }
-  )
-    .sort({ createAt: -1 })
+// const findLastFacultyId = async (): Promise<string | undefined> => {
+//   const lastFacultyId = await User.findOne(
+//     { role: 'faculty' },
+//     { id: 1, _id: 0 }
+//   )
+//     .sort({ createAt: -1 })
+//     .lean();
+
+//   return lastFacultyId?.id ? lastFacultyId.id.substring(2) : undefined;
+// };
+
+// export const generateFacultyId = async (): Promise<string> => {
+//   const lastFacultyId = await findLastFacultyId();
+//   const currentFacultyId = lastFacultyId || (0).toString().padStart(5, '0');
+//   let incrementedId = (parseInt(currentFacultyId) + 1)
+//     .toString()
+//     .padStart(5, '0');
+
+//   incrementedId = `F-${incrementedId}`;
+
+//   return incrementedId;
+// };
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne({ role: 'faculty' }, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
     .lean();
 
-  return lastFacultyId?.id ? lastFacultyId.id.substring(2) : undefined;
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
 };
 
 export const generateFacultyId = async (): Promise<string> => {
-  const lastFacultyId = await findLastFacultyId();
-  const currentFacultyId = lastFacultyId || (0).toString().padStart(5, '0');
-  let incrementedId = (parseInt(currentFacultyId) + 1)
-    .toString()
-    .padStart(5, '0');
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
 
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
   incrementedId = `F-${incrementedId}`;
-
   return incrementedId;
 };
